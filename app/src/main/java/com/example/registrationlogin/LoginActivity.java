44d58f5d487e4a13c2f57410d8bf6d9f4775a0bd
Rabia790 <rabiaangel9921@gmail.com>
2022-12-03 22:35:07 -0500

    Added login screen and added validations and toast

--- app/src/main/java/com/example/registrationlogin/LoginActivity.java
+++ app/src/main/java/com/example/registrationlogin/LoginActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -30,5 +32,32 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginBtn();
+            }
+        });
+
+    }
+    public void loginBtn(){
+        if (email.getText().toString().equals("")) {
+            Toast.makeText(this, "Please enter your Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (password.getText().toString().equals("")) {
+            Toast.makeText(this, "Please enter your Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        for (int i = 0; i < GeneralApplication.commonList.size(); i++) {
+            if (!email.getText().toString().equalsIgnoreCase(GeneralApplication.getList().get(i).getEmail()) || !password.getText().toString().equalsIgnoreCase(GeneralApplication.getList().get(i).getPassword())) {
+                Toast.makeText(this, "email and password didn't match", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                Toast.makeText(this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                //textToSpeech.speak(GeneralApplication.getList().get(i).getUsername() + "Login Successfully", TextToSpeech.QUEUE_FLUSH, null);
+                startActivity(new Intent(LoginActivity.this, MainActivity.class).putExtra("key", String.valueOf(i)));
+            }
+        }
     }
 }
--- app/src/main/java/com/example/registrationlogin/MainActivity.java
+++ app/src/main/java/com/example/registrationlogin/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.registrationlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView main;
+    String i = "";
+
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        main = findViewById(R.id.username);
+        i = getIntent().getStringExtra("key");
+        main.setText(GeneralApplication.getList().get(Integer.parseInt(i)).getUsername()
+                + "\n" + GeneralApplication.getList().get(Integer.parseInt(i)).getEmail()
+                + "\n" + GeneralApplication.getList().get(Integer.parseInt(i)).getPassword());
     }
 }
