f06a3431ce7084ab0ddfeab944bcc77334fa594b
Rabia790 <rabiaangel9921@gmail.com>
2022-12-03 20:01:30 -0500

    Added GeneralApplication and Modal class

--- app/src/main/java/com/example/registrationlogin/GeneralApplication.java
+++ app/src/main/java/com/example/registrationlogin/GeneralApplication.java
@@ -2,6 +2,24 @@ package com.example.registrationlogin;
 
 import android.app.Application;
 
-public class GeneralApplication extends Application {
+import java.util.ArrayList;
 
+public class GeneralApplication extends Application {
+    static ArrayList<Student> commonList;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        commonList = new ArrayList<>();
+    }
+
+    static public ArrayList<Student> getList() {
+
+        return commonList;
+    }
+
+    static public void setList(ArrayList<Student> list) {
+
+        commonList = list;
+    }
 }
