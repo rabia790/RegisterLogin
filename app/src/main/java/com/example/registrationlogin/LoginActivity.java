7e5b427174503f3f22ba7a19c0c51b61d8d9ad8a
Rabia790 <rabiaangel9921@gmail.com>
2022-12-03 21:25:03 -0500

    Created onclick Listener on signup Button

--- app/src/main/java/com/example/registrationlogin/LoginActivity.java
+++ app/src/main/java/com/example/registrationlogin/LoginActivity.java
@@ -2,13 +2,33 @@ package com.example.registrationlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button login, signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.btnLogin);
+        signup = findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
